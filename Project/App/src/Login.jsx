2ae10caab39eb667836cc64307244434edac758e
S4HIL100 <S.Bhandal2@wlv.ac.uk>
2026-03-01 15:49:12 +0000

    Added a button to see your password.

--- Project/App/src/Login.jsx
+++ Project/App/src/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import axios from "axios";
 import username from "./assets/username.png"
 import lock from "./assets/lock.png"
+import OpenLock from "./assets/Openlock.png"
 import { Link,Navigate } from 'react-router-dom';
 function Login() {
 	const [users, setUsers] = useState([]);
@@ -10,6 +11,9 @@ function Login() {
 	const [wrong_credential, setWrongCredential] = useState(true);
 	const [redirect, setRedirect] = useState(false);
 	const [show, setShow] = useState(false);
+	const [passwordState, setPasswordState] = useState("password");
+	const [image, setShowImage] = useState(lock);
+	const [count, setCount] = useState(0);
 
 	const login = async () => {
 		try {
@@ -43,6 +47,19 @@ function Login() {
 		setPassword(e.target.value);
 		setShow(false);
 	}
+	
+	function  PasswordStateChange(){
+		setCount(prev => prev + 1);
+		const newCount = count + 1;
+		if (newCount % 2 !== 0){
+			setShowImage(OpenLock);
+			setPasswordState("text");
+		}
+		else{
+			setShowImage(lock);
+			setPasswordState("password");
+		}
+	}
 
 	return (<>
 				{redirect && <Navigate to="/Home" />}
@@ -54,8 +71,8 @@ function Login() {
 					</div>
 					<br/>
 					<div>
-						<input className="Input3" type="password" placeholder="Password" value={password} onChange={PasswordChange}/>
-						<img className="user_img3" src={lock} alt=""></img>
+						<input className="Input3" type={passwordState} placeholder="Password" value={password} onChange={PasswordChange}/>
+						<button className = "img4" onClick = {PasswordStateChange}><img className = "img5" src={image} alt=""></img></button>
 					</div>
 					{show && (<p className = "CheckPassword">Username or password is incorrect.</p>)}
 					<button className="cred-button" onClick={login}>Login</button>
